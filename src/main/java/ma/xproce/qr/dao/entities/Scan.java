4158dac8e2cfa21f033faf99274b0c1ff3095026
Ayoub <ayoubmounadi50@gmail.com>
2025-02-26 03:02:56 +0000

    Jpa Version

--- src/main/java/ma/xproce/qr/dao/entities/Scan.java
+++ src/main/java/ma/xproce/qr/dao/entities/Scan.java
@@ -5,14 +5,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
+@Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Scan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
-
+    private long id;
+    private String userAgent;
+    private String ipAddress;
+    private LocalDateTime timestamp;
 }
--- src/main/java/ma/xproce/qr/service/QrScanService.java
+++ src/main/java/ma/xproce/qr/service/QrScanService.java
@@ -1,18 +1,27 @@
 package ma.xproce.qr.service;
 
+import ma.xproce.qr.dao.entities.Scan;
+import ma.xproce.qr.dao.repositorie.ScanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Service
 public class QrScanService {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private ScanRepository scanRepository;
 
     public void logScan(String ip, String userAgent) {
-        String sql = "INSERT INTO qr_scans (ip_address, user_agent, scanned_at) VALUES (?, ?, NOW())";
-        jdbcTemplate.update(sql, ip, userAgent);
+        System.out.println(ip);
+        System.out.println(userAgent);
+        Scan scan=Scan.builder().ipAddress(ip).userAgent(userAgent).timestamp(LocalDateTime.now()).build();
+        System.out.println(scan);
+        System.out.println(scanRepository.save(scan));
     }
 }
 
